04c920cad4044ff6f9e8fe546e00e7bfdddfefc8
davutD <durmaz.dvt@gmail.com>
2025-11-05 23:22:23 +0300

    Add an extensions.py file for cleaner structure and modify app.py file accordingly

--- app/app.py
+++ app/app.py
@@ -1,9 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from .config import Config
-
-db = SQLAlchemy()
+from .extensions import db, migrate, login_manager, bcrypt
 
 
 def create_app():
@@ -11,13 +8,14 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
     from app.auth.routes import auth
     from app.core.routes import core
 
     app.register_blueprint(core, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth")
-
-    migrate = Migrate(app, db)
 
     return app
